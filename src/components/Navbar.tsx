b911d606cb588c3ed15fc2c14ae0699fb33c6ef9
GunPKate <gunPK>
2024-04-03 22:15:58 +0700

    mycart items

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,9 +11,7 @@ export default function Navbar(){
     const { userTokens, updateUserToken } = useContext(UserTokenContext) as UserTokenContextType;
     
     const categories = ['Home','Product','Product2']
-    interface navProps{
-    value: string[]
-    }
+    const cart = `<svg width="40px" height="40px"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#fcfcfc" d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>    `;
 
     
     async function handleLogOut() {
@@ -27,7 +25,7 @@ export default function Navbar(){
 
     return (
     <>
-        <div className="bg-black min-h-[50px] text-white">
+        <div className="bg-black h-[56px] lg:h-[60px] text-white ">
             <div className="flex flex-inline justify-between ">
                 <div>
                     <span className="flex inline-flex ml-2">
@@ -45,7 +43,7 @@ export default function Navbar(){
 
                 </div>
 
-                <div>
+                <div className="flex">
                     {/* {userTokens[0].email.length } */}
                     {userTokens[0].email.length  === 0  ?
                         <><Link to={"/login"}>
@@ -63,7 +61,7 @@ export default function Navbar(){
                     }
                     <span>
                         <Link to={"/mycart"}>
-                            Mycart
+                            <div dangerouslySetInnerHTML={{__html: cart}}></div>
                         </Link>
                     </span>
                 </div>
--- src/pages/MyCart/MyCart.tsx
+++ src/pages/MyCart/MyCart.tsx
@@ -1,24 +1,96 @@
 export default function MyCart(){
 
-    function Card ( { children ,cardStyle, margin }:{ children: any , cardStyle: string, margin: string}){
+    function MainCard ( { children, cardStyle, width, title }:{ children: any , cardStyle: string, width: string, title: string}){
         return <>
-            <div className={cardStyle + margin}>
-             {children}
+            <div className={cardStyle + width}>
+                <div className="m-[24px] h-[32px]">
+                    {title}
+                </div> 
+                {children}
             </div>
         </>
     }
+
+    function ItemCard ( {name}:{ name:string} ){
+        return <>
+            <div className="mt-[24px] mb-[24px] max-h-[209px] max-w-[896px]">
+                <div className=" lg:flex block">
+
+                    <img src="https://picsum.photos/300/300.webp" className="w-[209px] h-[209px] object-cover" alt="" />
+
+                    <div className="lg:ml-[40px] w-full ">
+                        <div className="w-full h-[40px] flex justify-between">
+                            <div>
+                                {name}
+                            </div>
+                            <button>
+                                Delete
+                            </button>
+                        </div>
+
+                        <div className="relative flex w-full mt-[87px]">
+                            <div className="block lg:w-[139px] h-[82px] mr-[16px]">
+                                <label className="w-full h-[85px]">Color</label>
+                                <select className=" w-full h-[54px]">
+                                    <option>1</option>
+                                    <option>2</option>
+                                </select>
+                            </div>
+                            <div className="block lg:w-[139px] h-[82px] mr-[16px]">
+                                <label className="w-full h-[85px]">Size</label>
+                                <select className=" w-full h-[54px]">
+                                    <option>1</option>
+                                    <option>2</option>
+                                </select>
+                            </div>
+                            <div className="block lg:w-[139px] h-[82px] mr-[16px]">
+                                <label className="w-full h-[85px]">Qty</label>
+                                <select className=" w-full h-[54px]">
+                                    <option>1</option>
+                                    <option>2</option>
+                                </select>
+                            </div>
+      
+                            <div className="absolute bottom-0 right-0">
+                                <h1 className="">THB 2000</h1>
+                            </div>
+                        </div>
+                    </div>
+
+            
+
+                </div>
+            </div>
+            
+            <hr className="mb-[24px]"/>
+        </>
+    }
+
+    const cardStyleInput = "bg-red-300 w-full min-h-[800px] "
     return <>
-        <div className="lg:flex ">
-            <Card cardStyle={"bg-red-300 w-full min-h-[800px] "} margin={"ml-[16px]"} >
-                <div className="text-center bg-white mx-auto my-auto">
-                    Items
+        <div className={`lg:mx-[8.34%] mx-[0.834%]`}>
+        
+        <div className="lg:flex">
+                <div className="w-full ml-[24px] my-auto ">
+                    <div className="mt-[40px] mb-[40px]">
+                        My cart
+                    </div>
                 </div>
-                </Card>
-            <Card cardStyle={"bg-sky-300 w-full min-h-[800px] "} margin={"mr-[16px]"} >
-                <div className="text-center bg-white mx-auto my-auto">
-                    Summary
-                </div>
-            </Card>
+        </div>
+        <div className="lg:flex max-w-[1600px]">
+            <MainCard cardStyle={cardStyleInput} width="lg:max-w-[944px] " title="Items">
+            <div className="m-[24px] min-h-[490px] bg-white">
+                    <ItemCard name={"Item 1"}></ItemCard>
+                    <ItemCard name={"Item 2"}></ItemCard>
+                </div> 
+            </MainCard>
+            <MainCard cardStyle={cardStyleInput} width="lg:ml-[40px] lg:max-w-[616px] " title="Summary">
+                <div className="m-[24px] min-h-[420px] bg-white">
+                    123
+                </div> 
+            </MainCard>
+        </div>
+
         </div>
     </>
 }
