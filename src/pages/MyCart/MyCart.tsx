905c24af1d3bd15cd303dd98f4e01def09ee161c
GunPKate <gunPK>
2024-04-03 10:15:41 +0700

    mycart basic layout

--- src/pages/MyCart/MyCart.tsx
+++ src/pages/MyCart/MyCart.tsx
@@ -1,3 +1,24 @@
 export default function MyCart(){
-    return <>Mycart</>
+
+    function Card ( { children ,cardStyle, margin }:{ children: any , cardStyle: string, margin: string}){
+        return <>
+            <div className={cardStyle + margin}>
+             {children}
+            </div>
+        </>
+    }
+    return <>
+        <div className="lg:flex ">
+            <Card cardStyle={"bg-red-300 w-full min-h-[800px] "} margin={"ml-[16px]"} >
+                <div className="text-center bg-white mx-auto my-auto">
+                    Items
+                </div>
+                </Card>
+            <Card cardStyle={"bg-sky-300 w-full min-h-[800px] "} margin={"mr-[16px]"} >
+                <div className="text-center bg-white mx-auto my-auto">
+                    Summary
+                </div>
+            </Card>
+        </div>
+    </>
 }
