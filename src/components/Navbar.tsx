35ed5e205ecb0cb94c9248e6f5bc658087d7b70d
GunPKate <gunPK>
2024-04-07 18:50:26 +0700

    fix navbar style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar(){
 
     const { userTokens, updateUserToken } = useContext(UserTokenContext) as UserTokenContextType;
     const { myCartItems  } = useContext(MyCartItemContext) as MyCartItemContextType;
-    
+    const btnStyle = "my-auto bg-primary text-black p-2 rounded-md"
     // console.log("123",categories)
     const cart = `<svg width="40px" height="40px"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#fcfcfc" d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>    `;
 
@@ -28,8 +28,8 @@ export default function Navbar(){
 
     return (
     <>
-        <div className="bg-black h-[56px] lg:h-[60px] text-white ">
-            <div className="flex flex-inline justify-between mx-[160px]  ">
+        <div className="bg-black text-white ">
+            <div className="flex flex-inline h-[56px] lg:h-[60px] justify-between mx-[160px]  ">
                 <div className="my-auto">
                     <span className="flex inline-flex ml-2">
                         <Link to={`/`} >
@@ -46,10 +46,10 @@ export default function Navbar(){
 
                 </div>
 
-                <div className="flex mt-3 relative">
+                <div className="flex relative">
                     {/* {userTokens[0].email.length } */}
                     {userTokens[0].email.length  === 0  ?
-                        <><Link to={"/login"}>
+                        <><Link className={btnStyle} to={"/login"}>
                                 Log In
                             </Link>
                             <>
@@ -57,17 +57,19 @@ export default function Navbar(){
                                 {/* <span> &nbsp; {userTokens.length > 0 ? userTokens[0].email : "x"}</span> */}
                             </>
                         </>
-                        :<button onClick={()=>{ handleLogOut() }}>
+                        :<button className={btnStyle} onClick={()=>{ handleLogOut() }}>
                             Log out
                         </button>
 
                     }
-                    <span>
-                        <Link to={"/mycart"}>
-                            <div dangerouslySetInnerHTML={{__html: cart}}></div>
-                        </Link>
-                    </span>
-                    <span className="absolute right-3 text-red-500">{myCartItems.length}</span>
+                    <div className="flex my-auto ml-2">
+                        <span>
+                            <Link to={"/mycart"}>
+                                <div dangerouslySetInnerHTML={{__html: cart}}></div>
+                            </Link>
+                        </span>
+                        <span className="absolute right-3 text-red-500">{myCartItems.length}</span>
+                    </div>
                 </div>
             </div>
         </div>
